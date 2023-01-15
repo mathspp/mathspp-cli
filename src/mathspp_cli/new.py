46b22b237bc6172acc08bdcc1e5365bd15cad559
rodrigogiraoserrao <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2023-01-15 18:19:08 +0000

    Update command new.

--- src/mathspp_cli/new.py
+++ src/mathspp_cli/new.py
@@ -22,16 +22,76 @@ def to_slug(title: str) -> str:
     slug = "".join(char for char in title.lower().replace(" ", "-") if char in allow)
     return slug
 
+
 env.filters["to_slug"] = to_slug
+
+
+def _create_article(
+    title: str,
+    slug: str,
+    description: str,
+    tags: list[str],
+    categories: list[str],
+    path_prefix: str = "",
+):
+    date = datetime.datetime.now().strftime("%d-%m-%Y")
+    tags = sorted(set(tags))
+    categories = sorted(set(categories + ["blogpost"]))
+
+    frontmatter = env.get_template("frontmatter.yaml.template")
+    with open("C:/tmp/test.yaml", "w") as f:
+        f.write(
+            frontmatter.render(
+                categories=categories,
+                date=date,
+                description=description,
+                path_prefix=path_prefix,
+                tags=tags,
+                title=title,
+                slug=slug,
+            )
+        )
+
+    item = env.get_template("item.md.template")
+    with open("C:/tmp/item_test.yaml", "w") as f:
+        f.write(
+            item.render(
+                description=description,
+            )
+        )
 
 
 @app.command()
 def article(
     title: str = typer.Argument(..., help="The title for the blog article"),
-    tag: Optional[list[str]] = typer.Option(None, help="Tags for the article.", metavar="tag"),
-    category: Optional[list[str]] = typer.Option(None, help="Categories for the article.", metavar="category"),
+    slug: str = typer.Option("", help="Article slug."),
+    description: str = typer.Option("TODO", help="Article description.", prompt=True),
+    tag: list[str] = typer.Option(list, help="Tags for the article."),
+    category: list[str] = typer.Option(list, help="Categories for the article."),
 ):
-    frontmatter = env.get_template("frontmatter.yaml.template")
-    date = datetime.datetime.now().strftime("%d-%m-%Y")
-    with open("C:/tmp/test.yaml", "w") as f:
-        f.write(frontmatter.render(title=title, date=date, tags=tag, categories=category))
+    _create_article(
+        title=title,
+        slug=slug,
+        description=description,
+        tags=tag,
+        categories=category,
+    )
+
+
+@app.command()
+def pydont(
+    title: str = typer.Argument(..., help="The Pydon't title."),
+    slug: str = typer.Option("", help="Pydon't slug."),
+    description: str = typer.Option("TODO", help="Pydon't description.", prompt=True),
+    tag: list[str] = typer.Option(list, help="Tags for the Pydon't."),
+    category: list[str] = typer.Option(list, help="Categories for the Pydon't."),
+):
+    categories = category + ["pydont"]
+    tags = tag + ["python"]
+    _create_article(
+        title=title,
+        slug=slug,
+        description=description,
+        tags=tags,
+        categories=categories,
+    )
